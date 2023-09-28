1a759c5eb825401bf46594af9262c696fad3a0ab
Артем Чебыкин <riddle1994@ya.ru>
2023-09-28 09:47:30 +0300

    added parallel execution

--- src/test/java/CalcTests.java
+++ src/test/java/CalcTests.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+@Execution(ExecutionMode.CONCURRENT)
 public class CalcTests {
     private static CalcInt calc;
 
